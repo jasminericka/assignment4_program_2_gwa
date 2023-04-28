98a4485c6abc01e0779eba667b0e7344b065a1e2
jasminericka <jasmin.celebre@gmail.com>
2023-04-28 19:10:37 +0800

    for loop

--- program2.py
+++ program2.py
@@ -1,28 +1,16 @@
 #jasmin ericka a. celebre
 #bscpe 1-4
 
-#open the file
-file = open("student_gwa.txt","r")
-
-#initialize the highest GWA and the student who got it
-highest_gwa = 0
-name = ""
-
-#Loop through each line of the file
-for line in file:
-    #split the line
-    student,gwa = line.split(" ")
-    #convert the gwa to a float
-    gwa = float(gwa)
-
-    #check if the gwa is higher than the previous highest gwa
-    if gwa > highest_gwa:
-        #update the highest gwa and the student who got it
-        highest_gwa = gwa
-        name = student
-
-#print the student who got the highest gwa
-print("The student who got the highest GWA is {} with GWA of {}".format(name,highest_gwa))
-
-#close the file
-file.close()
+#open file
+with open ("student_gwa.txt", "r") as my_file:
+#initialize the name and highest gwa
+    highest_gwa = 0
+    student_name= " "
+#loop through the file
+    for line in my_file:
+        #split
+        student, gwa = line.split(" ")
+        #float gwa
+        gwa = float (gwa)
+#check the gwas
+#print output

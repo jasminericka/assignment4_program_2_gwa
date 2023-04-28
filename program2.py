244f87e0f6aab894d1beb35865d6f913e0d43d89
jasminericka <jasmin.celebre@gmail.com>
2023-04-28 19:21:37 +0800

    check gwas

--- program2.py
+++ program2.py
@@ -9,8 +9,12 @@ with open ("student_gwa.txt", "r") as my_file:
 #loop through the file
     for line in my_file:
         #split
-        student, gwa = line.split(" ")
+        student,gwa = line.split(" ")
         #float gwa
         gwa = float (gwa)
 #check the gwas
+    if gwa> highest_gwa:
+        highest_gwa= gwa
+        student_name= student
 #print output
+print ("The student who obtained the highest general weighted average is {} with a gwa of {}".format(student_name,highest_gwa))
